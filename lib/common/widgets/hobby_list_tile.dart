f036f019fc4ee65ab2ac7dde846e9abdd431f168
zTomz <tomvogel0@gmail.com>
2024-09-20 14:37:34 +0200

    Updated hobby list tile

--- lib/common/widgets/hobby_list_tile.dart
+++ lib/common/widgets/hobby_list_tile.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_lucide/flutter_lucide.dart';
 import 'package:schulplaner/common/constants/numbers.dart';
@@ -18,58 +17,102 @@ class HobbyListTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(
-        vertical: Spacing.small,
-        horizontal: Spacing.medium,
-      ),
-      child: Material(
-        color: Theme.of(context).colorScheme.surface,
-        borderRadius: const BorderRadius.all(Radii.small),
-        clipBehavior: Clip.hardEdge,
-        child: SizedBox(
-          height: 80,
-          child: InkWell(
-            onTap: () => onEdit(),
-            child: Row(
-              children: [
-                Container(
-                  width: 15,
-                  color: hobby.color,
-                ),
-                const SizedBox(width: Spacing.large),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.start,
+    return Align(
+      child: ConstrainedBox(
+        constraints: const BoxConstraints(
+          maxWidth: 800,
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            vertical: Spacing.small,
+            horizontal: Spacing.medium,
+          ),
+          child: Material(
+            color: Theme.of(context).colorScheme.surfaceContainer,
+            borderRadius: const BorderRadius.all(Radii.small),
+            clipBehavior: Clip.hardEdge,
+            child: SizedBox(
+              height: 80,
+              child: InkWell(
+                onTap: () => onEdit(),
+                child: Row(
                   children: [
-                    Text(
-                      hobby.name,
-                      style: Theme.of(context).textTheme.headlineSmall,
+                    Container(
+                      width: 15,
+                      color: hobby.color,
                     ),
-                    if (hobby.description != null)
-                      Text(
-                        hobby.description!,
-                        style: Theme.of(context).textTheme.bodyMedium,
+                    const SizedBox(width: Spacing.large),
+                    Expanded(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            hobby.name,
+                            style: Theme.of(context).textTheme.headlineSmall,
+                          ),
+                          hobby.description != null
+                              ? Text(
+                                  hobby.description!,
+                                  style: Theme.of(context).textTheme.bodyMedium,
+                                )
+                              : SizedBox(
+                                  height: 30,
+                                  child: ListView.builder(
+                                    itemCount: hobby.days.length,
+                                    scrollDirection: Axis.horizontal,
+                                    itemBuilder: (context, index) {
+                                      final currentDay = hobby.days[index];
+
+                                      return Container(
+                                        padding: const EdgeInsets.symmetric(
+                                          horizontal: Spacing.medium,
+                                        ),
+                                        margin: const EdgeInsets.only(
+                                          right: Spacing.small,
+                                        ),
+                                        decoration: BoxDecoration(
+                                          color: hobby.color.withOpacity(0.3),
+                                          borderRadius:
+                                              BorderRadius.circular(360),
+                                        ),
+                                        alignment: Alignment.center,
+                                        child: Text(
+                                          "${currentDay.day.name.substring(0, 2)}. ${currentDay.timeSpan.toFormattedString()}",
+                                          style: Theme.of(context)
+                                              .textTheme
+                                              .bodyMedium!
+                                              .copyWith(
+                                                color: hobby.color,
+                                                fontWeight: FontWeight.bold,
+                                              ),
+                                        ),
+                                      );
+                                    },
+                                  ),
+                                ),
+                        ],
                       ),
+                    ),
+                    const SizedBox(width: Spacing.medium),
+                    Tooltip(
+                      message: "Hobby bearbeiten",
+                      child: Icon(
+                        LucideIcons.pencil,
+                        color: Theme.of(context).colorScheme.onSurface,
+                      ),
+                    ),
+                    const SizedBox(width: Spacing.small),
+                    IconButton(
+                      onPressed: () => onDelete(),
+                      tooltip: "Hobby löschen",
+                      color: Theme.of(context).colorScheme.onSurface,
+                      icon: const Icon(LucideIcons.trash_2),
+                    ),
+                    const SizedBox(width: Spacing.large),
                   ],
                 ),
-                const Spacer(),
-                Tooltip(
-                  message: "Hobby bearbeiten",
-                  child: Icon(
-                    LucideIcons.pencil,
-                    color: Theme.of(context).colorScheme.onSurface,
-                  ),
-                ),
-                const SizedBox(width: Spacing.small),
-                IconButton(
-                  onPressed: () => onDelete(),
-                  tooltip: "Hobby löschen",
-                  color: Theme.of(context).colorScheme.onSurface,
-                  icon: const Icon(LucideIcons.trash_2),
-                ),
-                const SizedBox(width: Spacing.large),
-              ],
+              ),
             ),
           ),
         ),
