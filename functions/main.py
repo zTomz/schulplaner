b15ca93c3c2ac255753b023e15e7a45baec24830
zTomz <tomvogel0@gmail.com>
2024-10-28 19:05:00 +0100

    Delete functions folder

--- functions/main.py
+++
@@ -1,6 +0,0 @@
-# The Cloud Functions for Firebase SDK to create Cloud Functions and set up triggers.
-from firebase_functions import firestore_fn, https_fn
-
-# The Firebase Admin SDK to access Cloud Firestore.
-from firebase_admin import initialize_app, firestore, credentials
-import google.cloud.firestore
