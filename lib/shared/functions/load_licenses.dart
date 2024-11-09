ac3e0fa18b198d3117a1a88110f433b71c125b28
zTomz <tomvogel0@gmail.com>
2024-11-09 15:16:59 +0100

    Add license for lucide_icons to app

--- lib/shared/functions/load_licenses.dart
+++ lib/shared/functions/load_licenses.dart
@@ -5,5 +5,8 @@ Future<void> loadLicenses() async {
   return LicenseRegistry.addLicense(() async* {
     final appIconLicense = await rootBundle.loadString('assets/licenses/app_icon.txt');
     yield LicenseEntryWithLineBreaks(['app_icon'], appIconLicense);
+
+    final lucideIconsLicense = await rootBundle.loadString('assets/licenses/lucide_icons.txt');
+    yield LicenseEntryWithLineBreaks(['lucide_icons'], lucideIconsLicense);
   });
 }
